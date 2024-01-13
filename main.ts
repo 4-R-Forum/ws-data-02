a85d0a63cd4632ebd68ad0d5ac543e02e0d50478
4-R-Forum <jhodge@4-R-Forum.org>
2024-01-13 18:50:05 -0500

    copied ws-data-01 to use weatherbit 0.0.12 from weatherbit-experiment

--- main.ts
+++ main.ts
@@ -1,19 +1,37 @@
+/**
+ * WS Transmit
+ * 
+ * Downoad to GOLD
+ */
 input.onButtonPressed(Button.A, function () {
-    item += 1
-})
-let item = 0
-weatherbit.startWeatherMonitoring()
-basic.forever(function () {
-    if (item == 0) {
-        basic.showNumber(Math.idiv(weatherbit.humidity(), 1024))
-        basic.showString("Temp C: ")
-        basic.showNumber(Math.idiv(weatherbit.temperature(), 100))
-    } else if (item == 1) {
-        basic.showString("Humidity %: ")
-    } else if (item == 2) {
-        basic.showString("r=")
-        basic.showNumber(weatherbit.pressure())
-    } else {
-        item = 0
+    if (tx_power < 7) {
+        tx_power += 1
     }
+    basic.showNumber(tx_power)
+})
+input.onButtonPressed(Button.B, function () {
+    if (tx_power > 0) {
+        tx_power += -1
+    }
+    basic.showNumber(tx_power)
+})
+let data_string = ""
+let tx_power = 0
+let period = 300000
+tx_power = 7
+radio.setTransmitPower(tx_power)
+radio.setGroup(1)
+radio.setFrequencyBand(0)
+weatherbit.startWeatherMonitoring()
+weatherbit.startWindMonitoring()
+weatherbit.startRainMonitoring()
+loops.everyInterval(period, function () {
+    data_string = "" + convertToText(Math.round(weatherbit.temperature() / 100)) + "," + convertToText(Math.round(weatherbit.humidity() / 1024)) + "," + convertToText(Math.idiv(weatherbit.pressure(), 696)) + "," + convertToText(weatherbit.windSpeed()) + "," + weatherbit.windDirection()
+    radio.sendString(data_string)
+    basic.showString(data_string)
+    data_string = convertToText(weatherbit.rain())
+    radio.sendString(data_string)
+    basic.showString(data_string)
+    basic.showString("Wait")
+    basic.clearScreen()
 })
