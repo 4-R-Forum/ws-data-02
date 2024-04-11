069cdd27869442c2a0233ca0dfa6b66a019df801
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2024-04-11 17:31:23 -0400

    Update Period, Day Num for BenchTest

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-/**
- * WS Transmit
- * 
- * Downoad to GOLD
- */
 input.onButtonPressed(Button.A, function () {
     if (tx_power < 7) {
         tx_power += 1
@@ -15,13 +10,20 @@ input.onButtonPressed(Button.B, function () {
     }
     basic.showNumber(tx_power)
 })
+/**
+ * WS Transmit
+ * 
+ * Downoad to GOLD
+ */
 let data_string = ""
 let tx_power = 0
 let period = 60000
+let p_per_day = 2
 tx_power = 7
 radio.setTransmitPower(tx_power)
 radio.setGroup(1)
 radio.setFrequencyBand(0)
+let this_period = 0
 weatherbit.startWeatherMonitoring()
 weatherbit.startWindMonitoring()
 weatherbit.startRainMonitoring()
@@ -29,9 +31,13 @@ loops.everyInterval(period, function () {
     data_string = "" + convertToText(Math.round(weatherbit.temperature() / 100)) + "," + convertToText(Math.round(weatherbit.humidity() / 1024)) + "," + convertToText(Math.idiv(weatherbit.pressure(), 25600)) + "," + convertToText(Math.trunc(weatherbit.windSpeed())) + "," + weatherbit.windDirection()
     radio.sendString(data_string)
     basic.showString(data_string)
-    data_string = convertToText(weatherbit.rain())
-    radio.sendString(data_string)
-    basic.showString(data_string)
-    basic.showString("Wait")
-    basic.clearScreen()
+    this_period += 1
+    if (this_period == p_per_day) {
+        data_string = convertToText(weatherbit.rain())
+        radio.sendString(data_string)
+        basic.showString(data_string)
+        basic.showString("Wait")
+        basic.clearScreen()
+        this_period = 0
+    }
 })
