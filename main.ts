8143d4e1fef7999e4abd273830c3c90c6f4cc240
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2024-04-11 17:54:14 -0400

    Ready for Field Test 0411

--- main.ts
+++ main.ts
@@ -17,8 +17,8 @@ input.onButtonPressed(Button.B, function () {
  */
 let data_string = ""
 let tx_power = 0
-let period = 60000
-let p_per_day = 2
+let period = 300000
+let p_per_day = 288
 tx_power = 7
 radio.setTransmitPower(tx_power)
 radio.setGroup(1)
@@ -30,14 +30,10 @@ weatherbit.startRainMonitoring()
 loops.everyInterval(period, function () {
     data_string = "" + convertToText(Math.round(weatherbit.temperature() / 100)) + "," + convertToText(Math.round(weatherbit.humidity() / 1024)) + "," + convertToText(Math.idiv(weatherbit.pressure(), 25600)) + "," + convertToText(Math.trunc(weatherbit.windSpeed())) + "," + weatherbit.windDirection()
     radio.sendString(data_string)
-    basic.showString(data_string)
     this_period += 1
     if (this_period == p_per_day) {
         data_string = convertToText(weatherbit.rain())
         radio.sendString(data_string)
-        basic.showString(data_string)
-        basic.showString("Wait")
-        basic.clearScreen()
         this_period = 0
     }
 })
