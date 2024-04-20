a3b5eb95cc8705b472a744693395a709bf747101
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2024-04-20 12:41:10 -0400

    Build010

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
 def on_button_pressed_a():
-    global item
-    item += 1
+    global tx_power
+    if tx_power < 7:
+        tx_power += 1
+    basic.show_number(tx_power)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-item = 0
-weatherbit.start_weather_monitoring()
+def on_button_pressed_b():
+    global tx_power
+    if tx_power > 0:
+        tx_power += -1
+    basic.show_number(tx_power)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def on_forever():
-    global item
-    if item == 0:
-        basic.show_string("Temp C: ")
-        basic.show_number(Math.idiv(weatherbit.temperature(), 100))
-    elif item == 1:
-        basic.show_string("Humidity %: ")
-        basic.show_number(Math.idiv(weatherbit.humidity(), 1024))
-    elif item == 2:
-        basic.show_string("Pressure hPa: ")
-        basic.show_number(Math.idiv(weatherbit.pressure(), 25600))
-    else:
-        item = 0
-basic.forever(on_forever)
+def on_logo_pressed():
+    basic.show_string("010")
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+"""
+
+Build 010
+
+Desktop test 0420
+
+"""
+data_string = ""
+tx_power = 0
+period = 10000
+period_p_hour = 3
+tx_power = 7
+radio.set_transmit_power(tx_power)
+radio.set_group(1)
+radio.set_frequency_band(0)
+this_hour = 0
+this_period = 0
+# 12, less for test
+period_p_hour = 3
+# 24, less for test
+hours_p_day = 3
+weatherbit.start_weather_monitoring()
+weatherbit.start_wind_monitoring()
+weatherbit.start_rain_monitoring()
+
+def on_every_interval():
+    global data_string, this_period, this_hour
+    # String for radio > X characters.
+    data_string = "" + convert_to_text(Math.round(weatherbit.temperature() / 100)) + "," + convert_to_text(Math.round(weatherbit.humidity() / 1024)) + "," + convert_to_text(Math.idiv(weatherbit.pressure(), 25600)) + "," + convert_to_text(int(weatherbit.wind_speed())) + "," + weatherbit.wind_direction()
+    basic.show_string("" + convert_to_text(this_hour) + "-" + convert_to_text(this_period))
+    radio.send_string(data_string)
+    this_period += 1
+    if this_period > period_p_hour:
+        data_string = convert_to_text(weatherbit.rain())
+        radio.send_string(data_string)
+        this_period = 0
+        this_hour += 1
+        if this_hour > hours_p_day:
+            basic.show_string("Reset")
+            control.reset()
+loops.every_interval(period, on_every_interval)
--- main.ts
+++ main.ts
@@ -10,30 +10,45 @@ input.onButtonPressed(Button.B, function () {
     }
     basic.showNumber(tx_power)
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("010")
+})
 /**
- * WS Transmit
+ * Build 010
  * 
- * Downoad to GOLD
+ * Desktop test 0420
  */
 let data_string = ""
 let tx_power = 0
-let period = 300000
-let p_per_day = 288
+let period = 10000
+let period_p_hour = 3
 tx_power = 7
 radio.setTransmitPower(tx_power)
 radio.setGroup(1)
 radio.setFrequencyBand(0)
+let this_hour = 0
 let this_period = 0
+// 12, less for test
+period_p_hour = 3
+// 24, less for test
+let hours_p_day = 3
 weatherbit.startWeatherMonitoring()
 weatherbit.startWindMonitoring()
 weatherbit.startRainMonitoring()
 loops.everyInterval(period, function () {
+    // String for radio > X characters.
     data_string = "" + convertToText(Math.round(weatherbit.temperature() / 100)) + "," + convertToText(Math.round(weatherbit.humidity() / 1024)) + "," + convertToText(Math.idiv(weatherbit.pressure(), 25600)) + "," + convertToText(Math.trunc(weatherbit.windSpeed())) + "," + weatherbit.windDirection()
+    basic.showString("" + convertToText(this_hour) + "-" + convertToText(this_period))
     radio.sendString(data_string)
     this_period += 1
-    if (this_period >= p_per_day) {
+    if (this_period > period_p_hour) {
         data_string = convertToText(weatherbit.rain())
         radio.sendString(data_string)
         this_period = 0
+        this_hour += 1
+        if (this_hour > hours_p_day) {
+            basic.showString("Reset")
+            control.reset()
+        }
     }
 })
